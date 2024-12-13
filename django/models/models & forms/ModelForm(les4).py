8b7fbca6935426e5f4a7c50970457c0fbf81682e
Amar Yasir <3marofficial249@gmail.com>
2024-12-13 07:43:50 +0200

    .

---
+++ django/models/models & forms/ModelForm(les4).py
@@ -0,0 +1,32 @@
+# 📑ModelForm:->
+# 💡it`s built-in django class for combining Model and Form classes.
+# 💡by use it , we can collect the data from the form and store it into the database.
+
+# 📑Steps:->
+
+# 1️⃣ after define the model , Import the it to use it in ModelForm in (forms.py).
+"""
+from .models import inputModel (the model)
+"""
+
+# 2️⃣ Create a new class that inherits from ModelForm in (forms.py).
+"""
+class inputModelForm(forms.ModelForm):
+    class Meta:
+        model = inputModel
+        fields = '__all__' (to select all fields)
+
+"""
+
+# 3️⃣Create a view to this ModelForm:->
+"""
+i can add some code to make simple process:->
+
+form = inputModelForm()
+if request.method == 'POST':
+
+    form = inputModelForm(request.POST)
+context= {"form" : form}
+return render(request, "form.html", context)
+
+"""
