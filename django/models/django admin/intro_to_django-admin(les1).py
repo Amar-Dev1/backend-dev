2bc57f35bb9ca9fa467a0d69767c60aaef65973c
Amar Yasir <3marofficial249@gmail.com>
2024-12-13 14:57:01 +0200

    .

---
+++ django/models/django admin/intro_to_django-admin(les1).py
@@ -0,0 +1,10 @@
+# 📑Django admin
+# 💡It`s an admin dashboard specifically for admins to add, update and delete the models and users.
+# 💡Also give permissions or revoke it . and see users`s history.
+
+
+# 📑how to create admin interface:->
+"""
+- in the terminal type :-> `python manage.py createsuperuser`
+- fill out the name, email and the password.
+"""
