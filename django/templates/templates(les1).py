dbd5dc6afa467e6a8d58f5c10d9a18edbd1a5bec
Amar Yasir <3marofficial249@gmail.com>
2024-12-14 11:36:27 +0200

    .

---
+++ django/templates/templates(les1).py
@@ -0,0 +1,14 @@
+# 📑Template:->
+"""
+💡it`s a text file (html file) for the django project.
+💡templates represent a `presentation layer` in the (3 tire architecture).
+"""
+
+
+# 📑Dynamic html content:->
+"""
+💡we can use django template language (DTL) or jinja2 to create dynamic html content
+💡DTL is a templating engine that allows us to insert python code into our html
+💡we can use DTL to display data from our models in our templates
+💡we can use DTL to create dynamic html content that changes based on user input or
+"""
