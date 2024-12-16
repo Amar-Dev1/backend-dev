bfe404468265f7a95cbedcd05ff7e4f932f40aaf
Amar Yasir <3marofficial249@gmail.com>
2024-12-16 10:14:42 +0200

    .

--- django/templates/templates_inheritance(les3).py
+++ django/templates/templates_inheritance(les3).py
@@ -2,8 +2,8 @@
 # 💡The child can add new content in addition to the parent content.
 # 💡To apply templates inheritance , there is tow main tags:->
 """
-1️⃣ extends:-> used to inherite contents from parent to the child. like (extends content from base to about page) . Ex:->
+1️⃣ extends:-> used to inherite contents from parent to the child. like (extends content from base to about page). Ex:->
 {% include "header.html" %}
-2️⃣ include:-> used to include some content in html page . like (include header compoenent in the base file) . Ex:->
+2️⃣ include:-> used to include some content in html page . like (include header compoenent in the base file). Ex:->
 {% extends "base.html" %}
 """
